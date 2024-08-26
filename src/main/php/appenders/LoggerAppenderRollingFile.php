8a3e28c89b34325bb5d2311fdc1553e4bd5cc26c
Thierry Blind <tbl0605@gmail.com>
2024-08-26 15:59:52 +0200

    PHP 8.1 compatibility fixes

--- src/main/php/appenders/LoggerAppenderRollingFile.php
+++ src/main/php/appenders/LoggerAppenderRollingFile.php
@@ -206,7 +206,7 @@ class LoggerAppenderRollingFile extends LoggerAppenderFile {
 		if(flock($this->fp, LOCK_EX)) {
 			
 			// Write to locked file
-			if(fwrite($this->fp, $string) === false) {
+			if(fwrite($this->fp, (string) $string) === false) {
 				$this->warn("Failed writing to file. Closing appender.");
 				$this->closed = true;
 			}
